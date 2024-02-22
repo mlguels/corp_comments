21c592beda0e0ba14dd5dea1e41596cc345aa20e
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-02-21 18:08:17 -0800

    Hashtags now fetch its company comment on click

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyrigjt by
+          <a target="_blank" href="http://bytegrad.com">
+            ByteGrad.com
+          </a>
+          . Intended for learning or your portfolio
+        </p>
+
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own teaching material.
+        </p>
+      </small>
+    </footer>
+  );
+}
